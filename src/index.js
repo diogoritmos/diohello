126b21446eac004fa95324cc90d1a9dfdf97fa86
Diogo Dourado <dcdourado@gmail.com>
2024-10-15 18:40:11 -0300

    feat: handle cors

--- src/index.js
+++ src/index.js
@@ -8,12 +8,56 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const corsHeaders = {
+	'Access-Control-Allow-Origin': 'https://2888337c.diostatic.pages.dev',
+	'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+	'Access-Control-Max-Age': '86400',
+};
+function handleOptions(request) {
+	// Make sure the necessary headers are present
+	// for this to be a valid pre-flight request
+	let headers = request.headers;
+	if (
+		headers.get('Origin') !== null &&
+		headers.get('Access-Control-Request-Method') !== null &&
+		headers.get('Access-Control-Request-Headers') !== null
+	) {
+		// Handle CORS pre-flight request.
+		// If you want to check or reject the requested method + headers
+		// you can do that here.
+		let respHeaders = {
+			...corsHeaders,
+			// Allow all future content Request headers to go back to browser
+			// such as Authorization (Bearer) or X-Client-Name-Version
+			'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers'),
+		};
+		return new Response(null, {
+			headers: respHeaders,
+		});
+	} else {
+		// Handle standard OPTIONS request.
+		// If you want to allow other HTTP Methods, you can do that here.
+		return new Response(null, {
+			headers: {
+				Allow: 'GET, HEAD, POST, OPTIONS',
+			},
+		});
+	}
+}
+
 export default {
 	async fetch(request, env, ctx) {
-		// returns Hello + the value of the name query parameter
-		const url = new URL(request.url);
-		const name = url.searchParams.get('name') || 'World';
+		let response;
+		if (request.method === 'OPTIONS') {
+			response = handleOptions(request);
+		} else {
+			// returns Hello + the value of the name query parameter
+			const url = new URL(request.url);
+			const name = url.searchParams.get('name') || 'World';
 
-		return new Response(`Hello ${name}!`);
+			response = new Response(`Hello ${name}!`);
+		}
+
+		return response;
 	},
 };
