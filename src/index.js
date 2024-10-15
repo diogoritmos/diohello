4853d80d8e25e201d66501dbc4ad87b3c611e8cf
Diogo Dourado <dcdourado@gmail.com>
2024-10-15 19:35:20 -0300

    feat: add hono

--- src/index.js
+++ src/index.js
@@ -1,67 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
 
-const corsHeaders = {
-	'Access-Control-Allow-Origin': 'https://2888337c.diostatic.pages.dev',
-	'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
-	'Access-Control-Max-Age': '86400',
-};
-function handleOptions(request) {
-	// Make sure the necessary headers are present
-	// for this to be a valid pre-flight request
-	let headers = request.headers;
-	if (
-		headers.get('Origin') !== null &&
-		headers.get('Access-Control-Request-Method') !== null &&
-		headers.get('Access-Control-Request-Headers') !== null
-	) {
-		// Handle CORS pre-flight request.
-		// If you want to check or reject the requested method + headers
-		// you can do that here.
-		let respHeaders = {
-			...corsHeaders,
-			// Allow all future content Request headers to go back to browser
-			// such as Authorization (Bearer) or X-Client-Name-Version
-			'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers'),
-		};
-		return new Response(null, {
-			headers: respHeaders,
-		});
-	} else {
-		// Handle standard OPTIONS request.
-		// If you want to allow other HTTP Methods, you can do that here.
-		return new Response(null, {
-			headers: {
-				Allow: 'GET, HEAD, POST, OPTIONS',
-			},
-		});
-	}
-}
+// export default {
+// 	async fetch(request, env, ctx) {
+// 		let response;
+// 		if (request.method === 'OPTIONS') {
+// 			response = handleOptions(request);
+// 		} else {
+// 			// returns Hello + the value of the name query parameter
+// 			const url = new URL(request.url);
+// 			const name = url.searchParams.get('name') || 'World';
 
-export default {
-	async fetch(request, env, ctx) {
-		let response;
-		if (request.method === 'OPTIONS') {
-			response = handleOptions(request);
-		} else {
-			// returns Hello + the value of the name query parameter
-			const url = new URL(request.url);
-			const name = url.searchParams.get('name') || 'World';
+// 			response = new Response(`Hello ${name}!`);
+// 		}
 
-			response = new Response(`Hello ${name}!`);
-		}
+// 		return response;
+// 	},
+// 	async scheduled(controller, env, ctx) {
+// 		console.log('Scheduled event triggered');
+// 		// Add your scheduled event handling logic here
+// 	},
+// };
 
-		return response;
-	},
-	async scheduled(controller, env, ctx) {
-		console.log('Scheduled event triggered');
-		// Add your scheduled event handling logic here
-	},
-};
+const app = new Hono();
+
+app.get('/hello', (c) => {
+	return c.json({ a: 1 });
+});
+
+export default app;
